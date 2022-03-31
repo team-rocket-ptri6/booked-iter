3618b6e3d5d8f14fbb61744fee835b3ec9a65693
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-03-30 19:46:47 -0700

    Add database connection and createUser SQL

---
+++ server/models/queries.js
@@ -0,0 +1,8 @@
+queries = {};
+
+queries.createUser = `INSERT INTO users (first_name, last_name, email, user_name, password)
+    VALUES ($1, $2, $3, $4, $5)
+  RETURNING
+    user_id, user_name, first_name`;
+
+module.exports = queries;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,20 @@
 const express = require('express');
-
+const db = require('../models/database');
+const queries = require('../models/queries');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.status(200).json(res.locals);
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup', (req, res, next) => {
+  // db.query(queries.createUser, ['Jonathan', 'Haviv', 'jonathandhaviv@gmail.com', 'jonh', 'password'])
+  //   .then(response => res.locals = response.rows)
+  //   .then(() => next())
+  //   .catch(err => next({
+  //     log: err,
+  //   }));
+} , (req, res) => {
   res.status(200).json(res.locals);
 });
 
