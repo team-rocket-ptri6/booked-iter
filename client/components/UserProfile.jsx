d3a355cc6fc16f2766f713935cbc690ca2ab1aa5
Patrick Reid <patrickjreid@gmail.com>
2022-03-31 19:30:10 -0600

    created UserProfile.jsx page and NewClub.jsx page in components

---
+++ client/components/UserProfile.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+// import axios from 'axios';
+
+function UserProfile(){
+  // const {first_name, description, myFriends, myClubs} = useContext(UnnamedContext)
+
+  return (
+    <div>
+      <h1>user profile</h1>
+      <h2>User Image</h2> {/* stretch goal */}
+      
+      <h2>Welcome first_name</h2>
+      {/*  */}
+      <h2>Description</h2>
+      {/*  */}
+      <h2>My Friends</h2>
+      {/*  */}
+      <h2>My Clubs</h2>
+      {/*  */}
+      <button>Create New Club</button>
+      {/*  */}
+    </div>
+  );
+}
+
+export default UserProfile;
