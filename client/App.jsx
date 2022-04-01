557b2dccc15429efcd043979b1f0375c965a6143
Patrick Reid <patrickjreid@gmail.com>
2022-03-31 18:56:17 -0600

    Updated index.js to new React 18 format
    https://github.com/reactwg/react-18/discussions/5

--- client/App.jsx
+++ client/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div>Hello Goblin Sharks!</div>
+    <div>
+      <h1>Hello Goblin Sharks!</h1>
+      <UserProfile />
+    </div>
   );
 }
 
--- client/index.js
+++ client/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+// import { render } from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import App from './App';
 
-render(<App />, document.getElementById('app'));
+const container = document.getElementById('app');
+const root = ReactDOMClient.createRoot(container);
+
+root.render(<App />);
+
+// render(<App />, document.getElementById('app'));
+
