f8e056e5f379af1553f4966ab73c4325899cf429
Patrick Reid <patrickjreid@gmail.com>
2022-04-06 07:00:14 -0600

    Added several features regarding the club page most: Most work was done on the sub clubinfo page
    1) added a mock db
    2) description loads automatically using setState and a variable that holds state
    3) implemented map functionality to print members to screen
    4) added a check for if user is admin. If user is admin they will have:
      4.1) a form that allows them to add a member by email (code is for demo only)
      4.2) edit club page button
      4.3) when edit button is clicked it:
        - reveals a remove member button next to each member
        - reveals a edit description button under Edit description
    5) On clubPage added validation that someone must be a member to view
    
    to add before pushing to dev: - 'make admin' button next to each member when page is editable

--- client/components/ClubPage.jsx
+++ client/components/ClubPage.jsx
@@ -3,12 +3,21 @@ import ClubInfo from './ClubInfo';
 import ClubQuestions from './ClubQuestions';
 import ClubBookList from './ClubBookList';
 
+
+
 function ClubPage() {
+  const [isMember, setIsMember] = useState(true);
+
   return (
+    
     <div>
+      {!isMember ? 'We\'re sorry, but you are not a member of this club' :
+     ( <span>
       <ClubInfo />
-      <ClubQuestions />
-      <ClubBookList />
+      {/* <ClubQuestions /> */}
+      {/* <ClubBookList /> */}
+      </span>)
+      } 
     </div>
   );
 }
