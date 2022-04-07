220f2179d8a01034bf320d5fbd6747af7f2f3b0e
nidhikasireddy <nidhi.kasireddy@gmail.com>
2022-04-06 19:36:08 -0700

    submit working for create club on userprofile page

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -7,20 +7,43 @@ function UserProfile(){
   const [clubName, setClubName] = useState('');
   const [clubDescription, setClubDescription] = useState('');
   const [show, setShow] = useState(false);
+  const [clubs, setClubs] = useState([]);
+  
+  
   auth.firstName = 'Bob';
   auth.lastName = 'Saget';
   auth.description = 'I am super cool';
 
-  function createClub (e){
+  async function createClub (e){
     e.preventDefault();
     axios.post('http://localhost:8080/clubs/new', {
       clubName:clubName,
       clubDescription:clubDescription
+    }, {headers: {
+      'Authorization': `Bearer ${auth.token}` } 
     }).then((response) => {
-      console.log(response);
+      if (response) {
+        // const club_id = response.data.club_id; //use club_id as route path
+        // setClubs(clubs.concat({
+        //   id: response.data.club_id,
+        //   name: response.data.club_name,
+        //   description: response.data.description
+        // }));
+        // setClubs((prevClubs) => [
+        //   ...prevClubs,
+        //   {
+        //     id: response.data.club_id,
+        //     name: response.data.club_name,
+        //     description: response.data.description
+        //   },
+        // ]);
+        // console.log('THESE ARE YOUR CLUBS');
+        // console.log(clubs);
+      }
     });
   }
- 
+
+
   return (
     <div>
       <h1>user profile</h1>
@@ -34,16 +57,12 @@ function UserProfile(){
       {/* <h2>My Friends</h2> stretch goal
        */}
       <h2>My Clubs</h2>
-      {/*  */}
       <button onClick = {() => setShow(!show)}>Create New Club</button>
-      { show && <form style={{display: 'flex', flexDirection: 'column'}} onSubmit={(e) => createClub(e)} >
+      { show && <form style={{display: 'flex', flexDirection: 'column'}} id='createclub' onSubmit={(e) => createClub(e)} >
         <input type="text" placeholder="Club Name" value={clubName} onChange={(e)=> setClubName(e.target.value)}/>
         <textarea rows="4" cols="50" placeholder="Tell us about your club!" value={clubDescription} onChange={(e)=> setClubDescription(e.target.value)}></textarea>
-        <input type="text" placeholder="Add members" />
-        <button type='submit'>submit</button> <button onClick = {() => setShow(show)}>cancel</button> 
+        <button form='createclub' type='submit'  >submit</button> <button onClick = {() => setShow(show)}>cancel</button> 
       </form> }
-
-     
     </div>
   );
 }
