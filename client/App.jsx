402e8fbf6bd3cc57bd53f9c40a1d9aa9d2c4eedd
rmaiorca <79668744+rmaiorca@users.noreply.github.com>
2022-04-07 19:36:16 -0700

    Styling

--- client/App.jsx
+++ client/App.jsx
@@ -6,6 +6,7 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import './stylesheets/styles.css';
 import ClubPage from './components/ClubPage';
+import Book from './assets/book.png';
 
 function App() {
   const auth = useAuth();
@@ -16,16 +17,37 @@ function App() {
   };
 
   return (
-    <>
-      {!showLogin ? 
-        (<Signup />) :
-        (<Login />)
-      }
+    //If path is /profile then show userprofile plus all the other pages
+
+    //else render this below
+    <div className='book'>
+    <div className="back"><img className="logo" src={Book}/></div>
+
+    <div className="page10">
+      {!showLogin ?
+      (<Signup  />) :
+      (<Login />)
+    }
+ 
       {showLogin ?
-        (<p>Don&apos;t have an account? <span className='underline decoration-sky-500 cursor-pointer' type='button' onClick={toggleForm}>Sign up today!</span></p>) :
-        (<p>Have an account? <span className='underline decoration-sky-500 cursor-pointer' type='button' onClick={toggleForm}>Login here.</span></p>)
+        (<p className="text">Don&apos;t have an account? <span className="button" type='button' onClick={toggleForm}>Sign up today!</span></p>) :
+        (<p className="text">Have an account? <span className="button" type='button' onClick={toggleForm}>Login here</span></p>)
       }
-    </>
+    </div>
+    <div className="page9"></div>
+    <div className="page8"></div>
+    <div className="page7"></div>
+    <div className="page6"></div>
+    <div className="page5"></div>
+    <div className="page4"></div>
+    <div className="page3"></div>
+    <div className="page2"></div>
+    <div className="page1"></div>
+    <div className="front">
+    <img className="logo" src={Book}/>
+    </div>
+      
+    </div>
   );
 }
 
--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { useAuth } from '../auth/authContext'; 
+import Small from '../assets/smalllogo.png'
 
 function Signup() {
   const auth = useAuth();
@@ -14,23 +15,31 @@ function Signup() {
   }
 
   return (
-    <form className="place-self-center text-gray-800" onSubmit={e => handleSubmit(e)}>
-      <h1 className='font-bold text-xl mb-2 text-center'>Welcome to MyBookClub!</h1>
-      <div className="signup">
-        <label className="block text-gray-700 text-sm font-bold mb-2 pt-6">First Name</label>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={auth.firstName}onChange={(e)=>{auth.setFirstName(e.target.value);}}/>
-        <label className="block text-gray-700 text-sm font-bold mb-2 pt-6">Last Name</label>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={auth.lastName}onChange={(e)=>{auth.setLastName(e.target.value);}}/>
-        <label className="block text-gray-700 text-sm font-bold mb-2 pt-6">Email</label>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={auth.email}onChange={(e)=>{auth.setEmail(e.target.value);}}/>
-        <label className="block text-gray-700 text-sm font-bold mb-2 pt-6">Username</label>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" value={auth.username}onChange={(e)=>{auth.setUsername(e.target.value);}}/>
-        <label className="block text-gray-700 text-sm font-bold mb-2 pt-6">Password</label>
-        <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="password" value={auth.password}onChange={(e)=>{auth.setPassword(e.target.value);}}/>
-        <div><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 mt-6 px-4 rounded focus:outline-none focus:shadow-outline" type='submit'>Register</button></div>
-        
-      </div>
+    <div>
+      <img className="small" src={Small}/>
+      <h1 className="text" >Sign up here...</h1>
+    <form onSubmit={e => handleSubmit(e)}>
+      
+      <div className="formContainer">
+        {/* <label>First Name</label> */}
+        <input type="text" placeholder="First Name" value={auth.firstName}onChange={(e)=>{auth.setFirstName(e.target.value);}}/>
+        <br/>
+        {/* <label>Last Name</label> */}
+        <input type="text" placeholder="Last Name" value={auth.lastName}onChange={(e)=>{auth.setLastName(e.target.value);}}/>
+        <br/>
+        {/* <label>Email</label> */}
+        <input type="text" placeholder="Email" value={auth.email}onChange={(e)=>{auth.setEmail(e.target.value);}}/>
+        <br/>
+        {/* <label>Username</label> */}
+        <input type="text" placeholder="Username" value={auth.username}onChange={(e)=>{auth.setUsername(e.target.value);}}/>
+        <br/>
+        {/* <label >Password</label> */}
+        <input  type="password" placeholder ="Password" value={auth.password}onChange={(e)=>{auth.setPassword(e.target.value);}}/>
+        <div><button className="button" type='submit'>Register</button></div>
+        </div>
+      
     </form>
+    </div>
   );
 }
     
