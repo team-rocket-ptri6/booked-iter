cd07a96e064b1543be6cdb1a274f34a494a501bf
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-04-07 19:02:16 -0700

    Fixes username and password bug

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,9 +6,12 @@ const SALT_WORK_FACTOR = 10;
 
 const userController = {};
 
+const regex = new RegExp('\W');
+
 userController.createUser = async (req, res, next) => {
   try {
     const { username, password, email, firstName, lastName } = req.body;
+    if (!regex.test(username) || !regex.test(password)) throw 'Username and/or password are blank.';
     const hashPassword = await bcrypt.hash(password, SALT_WORK_FACTOR);
     const response = await db.query(queries.createUser, [firstName, lastName, email, username, hashPassword]);
     res.locals = response.rows[0];
@@ -24,6 +27,7 @@ userController.createUser = async (req, res, next) => {
 userController.loginUser = async (req, res, next) => {
   try {
     const { username, password } = req.body;
+    if (!regex.test(username) || !regex.test(password)) throw 'Username and/or password are blank.';
     const response = await db.query(queries.loginUser, [username]);
     const match = await bcrypt.compare(password, response.rows[0].password);
     if(!match) return res.status(403).send('You have entered invalid username or password.');
