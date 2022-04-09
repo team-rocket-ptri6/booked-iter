cfd214ab162452f26cb46435f38e726b7dd896d5
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-04-09 01:52:12 -0700

    Render Books

---
+++ client/components/Book.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function Book({title, authors, bookId, currentlyReading, thumbnail}) {
+  return (
+    <>
+      {!currentlyReading && <button type='button'>Vote to read next!</button>} <span>{title}</span> <img src={thumbnail} style={{'max-height': '208px'}}/>
+    </>
+  );
+}
+
+export default Book;
