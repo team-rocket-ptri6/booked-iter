bc6212e11e812ebdb27891a06437d50c81e606b6
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-04-09 00:18:13 -0700

    Saves books in search to database

--- client/components/BookSearch.jsx
+++ client/components/BookSearch.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import SearchSuggestions from './SearchSuggestions';
+import { useParams } from 'react-router-dom';
+import { useAuth } from '../auth/authContext';
 
 function BookSearch() {
+  const params = useParams();
+  const auth = useAuth();
+
   const [search, setSearch] = useState('');
   const [suggestions, setSuggestions] = useState('');
 
@@ -19,7 +24,7 @@ function BookSearch() {
   const fetchBooks = (query) => {
     if (query === '') return;
     query = query.replace(' ', '+');
-    return fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${query}&maxResults=5&sorting=relevance&projection=lite&fields=items(id,volumeInfo(title, authors,imageLinks(thumbnail)))`)
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${query}&maxResults=10&sorting=relevance&projection=lite&fields=items(id,volumeInfo(title, authors,imageLinks(thumbnail)))`)
       .then(response => response.json())
       .then(data => setSuggestions(data.items))
       .catch(err => {
@@ -32,15 +37,33 @@ function BookSearch() {
     debounce(fetchBooks(search), 100);
   }, [search]);
 
+  const saveBook = (bookId) => {
+    const body = JSON.stringify({
+      clubId: params.id,
+      googleBookId: bookId,
+    });
+
+    fetch('http://localhost:8080/books', {
+      method: 'POST',
+      headers: {
+        'Authorization':`Bearer ${auth.token}`,
+        'Content-Type': 'application/json'
+      },
+      body: body,
+    }).then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => 'Could not save the book');
+  };
+
 
 
   return (
     <>
       <div>
-        <input type="search" placeholder='Search for a new book to read!' id='search' size='30' aria-label='Search for a new book to read!' value={search} onChange={e => setSearch(e.target.value)}/> <button type='button' htmlFor='search'>Search</button>
+        <input type="search" placeholder='Search for a new book to read!' id='search' size='30' aria-label='Search for a new book to read!' value={search} onChange={e => setSearch(e.target.value)}/>
       </div>
       {Array.isArray(suggestions) && search != '' && suggestions.map(suggestion => {
-        return <SearchSuggestions title={suggestion.volumeInfo.title} author={suggestion.volumeInfo.authors}  key={suggestion.id}/>;
+        return <SearchSuggestions save={saveBook} title={suggestion.volumeInfo.title} author={suggestion.volumeInfo.authors}  key={suggestion.id} googleBookId={suggestion.id}/>;
       })}
     </>
   );
--- client/components/SearchSuggestions.jsx
+++ client/components/SearchSuggestions.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 
-function SearchSuggestions({ title, author }) {
+function SearchSuggestions({ title, author, googleBookId, save }) {
+  const [saved, setSaved] = useState(false);
+
+  function saveSuggestion() {
+    save(googleBookId);
+    setSaved(true);
+  }
   return (
     <div className='bg-white border-none'>
-      <span>{title} by {author}</span>
+      <span>{title} by {author}</span> {saved ? <span>Book Saved! ✅</span> : <button type='button' onClick={saveSuggestion}>Save to reading list!</button>}
     </div>
   );
 };
