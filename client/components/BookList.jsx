cb5c95bec0bdb8ce2236e3cbf09d40528d7fe6eb
Jonathan Haviv <jonathandhaviv@gmail.com>
2022-04-09 11:24:24 -0700

    Adds logo to shark on club page and conditionally renders currently reading

--- client/components/BookList.jsx
+++ client/components/BookList.jsx
@@ -18,9 +18,14 @@ function BookList({ readingList }) {
 
   return (
     <>
-      <label>Currently Reading: </label> {currentlyReading && currentlyReading.length > 0 && <Book title={currentlyReading.title} key={currentlyReading.bookId} bookId={currentlyReading.bookId} author={currentlyReading.authors} currentlyReading={true} thumbnail={currentlyReading.thumbnail ? currentlyReading.thumbnail.thumbnail : 'https://toppng.com/uploads/preview/book-cover-stock-photography-clip-art-stack-of-books-11563000775i3ijq3g55g.png'}/>}
+      {currentlyReading && currentlyReading.length > 0 &&
+      <>
+        <label>Currently Reading: </label>  <Book title={currentlyReading.title} key={currentlyReading.bookId} bookId={currentlyReading.bookId} author={currentlyReading.authors} currentlyReading={true} thumbnail={currentlyReading.thumbnail ? currentlyReading.thumbnail.thumbnail : 'https://toppng.com/uploads/preview/book-cover-stock-photography-clip-art-stack-of-books-11563000775i3ijq3g55g.png'}/>
+      </>
+      }
       {rank && rank.length > 0 &&
           <div>
+            <p>What should we read next?</p>
             <ol>
               {rank.map(book => {
                 return (<li key={book.google_book_id}>
