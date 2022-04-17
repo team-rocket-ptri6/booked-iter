926358b575c7f3b1a216e3bd01c8d5678356411c
Gerry Bong <ggbong734@gmail.com>
2022-04-16 18:11:35 -0700

    added loading message for when Book List is still loading

--- client/components/Book.jsx
+++ client/components/Book.jsx
@@ -1,15 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-function Book({title, authors, bookId, currentlyReading, thumbnail, readNow}) {
+function Book({
+  title,
+  authors,
+  bookId,
+  currentlyReading,
+  thumbnail,
+  readNow,
+}) {
   return (
     <>
-      <span >{title}</span> 
-      
-      {!currentlyReading &&
-      <>
-        <button className = "voteButton" type='button'>Vote to read next!</button> <button onClick={readNow} className = "voteButton" type='button'>We are reading this!</button>
-      </> 
-      } <div> <img src={thumbnail} style={{'maxHeight': '208px'}}/></div>
+      <span className="font-semibold text-lg">{title}</span>
+      {!currentlyReading && (
+        <>
+          <button className="voteButton" type="button">
+            Vote to read next!
+          </button>{" "}
+          <button onClick={readNow} className="voteButton" type="button">
+            We are reading this!
+          </button>
+        </>
+      )}{" "}
+      <div>
+        {" "}
+        <img src={thumbnail} style={{ maxHeight: "208px" }} />
+      </div>
     </>
   );
 }
