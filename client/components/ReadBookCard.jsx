411f8c5eb2e13cd4dfe53366dcaad0cd557ab3dc
Gerry Bong <ggbong734@gmail.com>
2022-04-17 23:33:45 -0700

    added my rating and average club rating texts to readbookcard. added a collapsible section to edit rating.

--- client/components/ReadBookCard.jsx
+++ client/components/ReadBookCard.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../auth/authContext";
+import { ReadBookRatingModal } from "./ReadBookRatingModal";
 
 export const ReadBookCard = ({
   book,
@@ -13,6 +14,8 @@ export const ReadBookCard = ({
     const date = new Date(datetime);
     return date.toDateString();
   };
+
+  const [showRatingModal, setShowRatingModal] = useState(false);
 
   const deleteBook = (bookId) => {
     fetch(`http://localhost:8080/books/delete/${bookId}`, {
@@ -34,6 +37,8 @@ export const ReadBookCard = ({
       .catch((err) => console.warn(err));
   };
 
+  const openRatingModal = () => {};
+
   console.log("book object is:", book);
 
   return (
@@ -48,6 +53,7 @@ export const ReadBookCard = ({
           }
           alt=""
         />
+        {/* {"the text section of the card"} */}
         <div className="p-6 flex flex-col justify-start w-full">
           <div className="flex justify-end items-end">
             <button
@@ -70,10 +76,10 @@ export const ReadBookCard = ({
             {book.date_read ? timeConverter(book.date_read) : "not available"}
           </p>
           <div className="flex">
-            <p className="text-gray-600 text-sm">My rating :</p>
-            <ul className="flex justify-center list-none">
-              {book.rating &&
-                Array(book.rating)
+            <p className="text-gray-600 text-sm">My rating: </p>
+            {book.rating ? (
+              <ul className="flex justify-center list-none">
+                {Array(book.rating)
                   .fill(1)
                   .map((n, index) => (
                     <li key={index + "star"}>
@@ -94,27 +100,59 @@ export const ReadBookCard = ({
                       </svg>
                     </li>
                   ))}
-            </ul>
+              </ul>
+            ) : (
+              <p className="text-gray-600 text-sm">{" No rating yet"}</p>
+            )}
           </div>
-          <div className="flex items-center">
-            <svg
-              className="w-5 h-5 text-yellow-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-            </svg>
-            <p className="ml-2 text-sm font-bold text-gray-900 dark:text-white">
-              4.95
+          {/* {"Average club rating text"} */}
+          <div className="flex">
+            <p className="text-gray-600 text-sm">Average club rating: </p>
+            {book.avg_rating ? (
+              <>
+                <svg
+                  className="w-5 h-5 text-yellow-400"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                <p className="ml-2 text-sm font-bold text-gray-700">
+                  {book.avg_rating.toFixed(2)}
+                </p>
+                <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full "></span>
+                <a
+                  href="#"
+                  className="text-sm font-medium text-gray-700 underline hover:no-underline"
+                >
+                  {book.num_rating} reviews
+                </a>
+              </>
+            ) : (
+              <p className="text-gray-600 text-sm">{" No rating yet"}</p>
+            )}
+          </div>
+          <div className="flex">
+            <p className="text-gray-600 text-sm">My review: </p>
+            <p className="text-gray-600 text-sm">
+              {book.review ? book.review : "No review added"}
             </p>
-            <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
-            <a
-              href="#"
-              className="text-sm font-medium text-gray-900 underline hover:no-underline dark:text-white"
+          </div>
+          <div>
+            <button
+              type="button"
+              className="inline-block px-1 py-0.5 border-1 border-blue-800 cursor-pointer
+              text-blue-700 font-medium text-xs leading-tight uppercase rounded
+              focus:outline-none focus:ring-0
+          transition duration-150 ease-in-out"
+              onClick={() => {
+                setShowRatingModal(!showRatingModal);
+              }}
             >
-              73 reviews
-            </a>
+              Edit my rating
+            </button>
+            {showRatingModal ? <ReadBookRatingModal /> : <></>}
           </div>
         </div>
       </div>
