d03d2c4a5762d50d9c4da5d1fc04a15c14d52bc1
Gerry Bong <ggbong734@gmail.com>
2022-04-17 23:33:45 -0700

    added my rating and average club rating texts to readbookcard. added a collapsible section to edit rating.

---
+++ client/components/ReadBookRatingModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const ReadBookRatingModal = () => {
+  return (
+    <div className="flex">
+      <div className="block p-6 rounded-lg shadow-lg bg-white">
+        Some placeholder content for the collapse component. This panel is
+        hidden by default but revealed when the user activates the relevant
+        trigger.
+      </div>
+    </div>
+  );
+};
