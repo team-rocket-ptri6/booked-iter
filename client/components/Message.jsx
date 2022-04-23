d57c2d786a8435d3ee0ccf6d55949133bbe21173
Gerry Bong <ggbong734@gmail.com>
2022-04-23 10:24:39 -0700

    changed import react statement in Message.jsx to resolve error, fixed className error in Message.jsx

--- client/components/Message.jsx
+++ client/components/Message.jsx
@@ -1,10 +1,12 @@
-import React from 'React';
+import React from "react";
 
 export default function Message({ username, message, created_at }) {
   return (
-    <div class="message">
+    <div className="message">
       <div className="message-header">
-        <p className="message-username"><b>{username}</b></p>
+        <p className="message-username">
+          <b>{username}</b>
+        </p>
         <p className="message-created">{created_at}</p>
       </div>
       <p className="message-content">{message}</p>
