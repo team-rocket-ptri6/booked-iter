c1bda8159dd989f268df0c8da9cc2aadf440b7ad
Gerry Bong <ggbong734@gmail.com>
2022-04-23 11:29:41 -0700

    fixed timestamp issue in Messages

--- client/components/Message.jsx
+++ client/components/Message.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 export default function Message({ username, message, created_at }) {
+  const timeFormatter = (datetime) => {
+    const date = new Date(datetime);
+    date.setHours(date.getHours() - 6);
+    return date.toLocaleString("en-US", { timeZone: "America/New_York" });
+  };
   return (
     <div className="message">
       <div className="message-header">
         <p className="message-username">
           <b>{username}</b>
         </p>
-        <p className="message-created">{created_at}</p>
+        <p className="message-created">{timeFormatter(created_at)}</p>
       </div>
       <p className="message-content">{message}</p>
     </div>
